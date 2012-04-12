9b5e3483b4ab1d28621ac432ef804f148a88d8b0
huilaner <huibiology@gmail.com>
2012-04-12 18:49:35 -0300

    add gcd by hui 1st commit

--- src/rsaChat/RSA.java
+++ src/rsaChat/RSA.java
@@ -6,5 +6,22 @@ package rsaChat;
  * @version Apr. 12 2012
  */
 public class RSA {
-    //
+    //   public static int coprime(int x) {
+    
+	return 0;
+	
+    }
+    
+    public static int GCD(int a, int b) {
+	int r;
+	int x = a;
+	int y = b;
+	while(y != 0) {
+	    r = x % y;
+	    x = y;
+	    y = r;
+	}
+	return x;
+	
+    }
 }
