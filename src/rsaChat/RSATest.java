fa0387e039650a1f120138d1f17f0ac372aed4a6
huilaner <huibiology@gmail.com>
2012-04-15 22:11:59 -0300

    updated unit tests for gcd, mod_inverse, modulo. It seems coprime is hard to test.

--- src/rsaChat/RSATest.java
+++ src/rsaChat/RSATest.java
@@ -3,6 +3,8 @@ package rsaChat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.math.BigInteger;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,22 +16,51 @@ public class RSATest {
 
     @Test
     public void testCoprime() {
-	fail("Not yet implemented"); // TODO
+	//since it is randomly generate number, it is hard to test
+	//so shall we delete this test?
     }
 
     @Test
     public void testGCD() {
-	fail("Not yet implemented"); // TODO
+	assertEquals(1, RSA.GCD(3, 8));
+	assertEquals(3, RSA.GCD(3, 9));
+	assertEquals(1, RSA.GCD(1, 0));
+	assertEquals(RSA.GCD(100, 10), 10);
+	assertEquals(RSA.GCD(10, 100), 10);
+	assertEquals(RSA.GCD(100, 13), 1);
+	assertEquals(RSA.GCD(100, 100), 100);
+	assertEquals(RSA.GCD(13, 39), 13);
+	assertEquals(RSA.GCD(120, 35), 5);
+	assertEquals(RSA.GCD(1001, 77), 77);
+	assertEquals(RSA.GCD(1001, 33), 11);
     }
 
     @Test
     public void testMod_inverse() {
-	fail("Not yet implemented"); // TODO
+	assertEquals(
+		RSA.mod_inverse(BigInteger.valueOf(3), BigInteger.valueOf(17)),
+		BigInteger.valueOf(6));
+	assertEquals(
+		RSA.mod_inverse(BigInteger.valueOf(55), BigInteger.valueOf(123)),
+		BigInteger.valueOf(85));
+	assertEquals(
+		RSA.mod_inverse(BigInteger.valueOf(45), BigInteger.valueOf(223)),
+		BigInteger.valueOf(114));
+	assertEquals(
+		RSA.mod_inverse(BigInteger.valueOf(2342), BigInteger.valueOf(92830457)),
+		BigInteger.valueOf(75588250));
+	assertEquals(
+		RSA.mod_inverse(BigInteger.valueOf(3), BigInteger.valueOf(30)),
+		BigInteger.valueOf(0));
     }
 
     @Test
     public void testModulo() {
-	fail("Not yet implemented"); // TODO
+	assertEquals(RSA.modulo(2, 0, 55),1);
+	assertEquals(RSA.modulo(2, 4, 1000),16);
+	assertEquals(RSA.modulo(2, 6, 5),4);
+	assertEquals(RSA.modulo(89, 1232, 4623),1);
+	assertEquals(RSA.modulo(2349723, 423424, 12345),696);
     }
 
     @Test
