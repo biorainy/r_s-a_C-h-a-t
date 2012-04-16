eefaa28d406fa439ed62a88f76ee3c6c7219c7f3
huilaner <huibiology@gmail.com>
2012-04-16 17:23:37 -0300

    add real function to client

--- src/rsaChat/Client.java
+++ src/rsaChat/Client.java
@@ -1,13 +1,40 @@
 package rsaChat;
 
+
+import java.net.*;
+import java.io.*;
+
 public class Client {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
 
+
+    /**
+     * JavaProgrammingForums.com
+     */
+    public static void main(String[] args) throws Exception {
+	// Port to monitor
+	final int myPort = 1071;
+	//Client client = new Client();
+	InetAddress serverIPAddr = InetAddress.getLocalHost();
+	Socket csock = new Socket(serverIPAddr,myPort);
+	DataOutputStream out = new DataOutputStream(csock.getOutputStream());
+	
+	SendMsg send = new SendMsg(out);
+	send.start();
+	
+	BufferedReader in = new BufferedReader(new InputStreamReader(csock.getInputStream()));
+	getRequest(in);
+	
+	
+
+    }
+    
+    static String getRequest(BufferedReader in) throws Exception {
+	String s = null;
+	while ((s = in.readLine()) != null) {
+	    System.out.println("got: " + s);
+	}
+	return s;
     }
 
 }
